b6525ed030714256ebd2cdad0a1713b40bce5d88
Solidshots <seanbrent5@gmail.com>
2021-06-24 18:33:32 -0400

    pwa functioning

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require("webpack")
 const path = require("path");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
@@ -44,6 +45,24 @@ module.exports = {
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: "static", //report outputs to an html file in dist
+    }),
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: "Foodies",
+      description: "An app that allows you to view upcoming food events.",
+      //homepage of the pwa, manifest file relative to the starting html file
+      start_url: "../index.html",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      //tells webpack to not create a new manifest code in the title
+      fingerprints: false,
+      //tells webpack to not inject the manifest.json into the HTML, should be used with fingerprints true
+      inject: false,
+      icons: [{
+        src: path.resolve("assets/img/icons/icon-512x512.png"),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join("assets", "icons")
+      }]
     })
   ],
   mode: "development"
